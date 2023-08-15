3e3a92e35d12c6d3457d0db5cb6b0177681a94aa
amir4774 <amirabbassi1382@gmail.com>
2023-08-15 22:32:16 +0330

    handle with TS

--- script.ts
+++ script.ts
@@ -1,39 +1,48 @@
 // Get and creat element
-const darkMode = document.getElementById('change-mode');
-const miliSecond = document.getElementById('mili-second');
-const second = document.getElementById('second');
-const minutes = document.getElementById('minutes');
-const start = document.getElementById('start');
-const lap = document.getElementById('lap');
-const table = document.querySelector('.table');
+const darkMode = document.getElementById('change-mode') as HTMLDivElement;
+const miliSecond = document.getElementById('mili-second') as HTMLDivElement;
+const second = document.getElementById('second') as HTMLDivElement;
+const minutes = document.getElementById('minutes') as HTMLDivElement;
+const start = document.getElementById('start') as HTMLDivElement;
+const lap = document.getElementById('lap') as HTMLDivElement;
+const table = document.querySelector('.table') as HTMLDivElement;
 let mili_sec = 0;
 let sec = 0;
 let min = 0;
 let numberOfLab = 1;
-let total = {
+let interval: ReturnType<typeof setInterval>;
+
+interface Clock {
+    minute: number,
+    seconds: number,
+    mili_seconds: number
+}
+
+let total: Clock = {
     minute: 0,
     seconds: 0,
-    mili_seceonds: 0
+    mili_seconds: 0
 };
 
 // Dark Mode
 darkMode.addEventListener('click', () => {
-    document.querySelector('.fas.fa-sun').classList.toggle('fa-moon');
+    const fa = document.querySelector('.fas.fa-sun') as HTMLElement;
+    fa.classList.toggle('fa-moon');
     document.body.classList.toggle('dark-mode');
 });
 
 // Return number => '02' or '22'
-const returnNumber = num => num < 10 ? "0" + num : num;
+const returnNumber = (num: number): string | number => num < 10 ? "0" + num : num;
 
 // Overall-time
-const calculateSum = (min, sec, mili_sec, total) => {
+const calculateSum = (min: number, sec: number, mili_sec: number, total: Clock): string => {
 
-    total.mili_seceonds += mili_sec;
+    total.mili_seconds += mili_sec;
     total.seconds += sec;
     total.minute += min;
     
-    if(total.mili_seceonds > 99) {
-        total.mili_seceonds = total.mili_seceonds % 100;
+    if(total.mili_seconds > 99) {
+        total.mili_seconds = total.mili_seconds % 100;
         total.seconds += 1;
     }
 
@@ -42,13 +51,13 @@ const calculateSum = (min, sec, mili_sec, total) => {
         total.minute += 1;
     }
 
-    return returnNumber(total.minute) + ' : ' + returnNumber(total.seconds) + ' . ' + returnNumber(total.mili_seceonds);
+    return returnNumber(total.minute) + ' : ' + returnNumber(total.seconds) + ' . ' + returnNumber(total.mili_seconds);
 
 };
 
 const startHandler = () => {
     // Lap button active
-    lap.style.opacity = 1;
+    lap.style.opacity = '1';
     lap.style.pointerEvents = 'visible';
 
     if(start.innerHTML === 'Start' || start.innerHTML === 'Resume') {
@@ -60,10 +69,10 @@ const startHandler = () => {
         interval = setInterval(() => { // Start Mode
             if(mili_sec < 99) {
                 mili_sec += 1;
-                miliSecond.innerHTML = returnNumber(mili_sec); 
+                miliSecond.innerHTML = returnNumber(mili_sec) + ''; 
             } else {
                 mili_sec = 0;
-                miliSecond.innerHTML = returnNumber(mili_sec);
+                miliSecond.innerHTML = returnNumber(mili_sec)+ '';
                 if(sec < 59) {
                     sec += 1;
                     second.innerHTML = returnNumber(sec) + ' . ';
@@ -96,7 +105,7 @@ const lapHandler = () => {
             <p>${calculateSum(min, sec, mili_sec, total)}</p>
         `;
         div.setAttribute('class', 'items');
-        div.setAttribute('id', numberOfLab);
+        div.setAttribute('id', `${numberOfLab}`);
         // Add new item to table 
         table.appendChild(div);
         numberOfLab++;
@@ -117,14 +126,14 @@ const lapHandler = () => {
         lap.style.pointerEvents = 'none';
         table.style.display = 'none';
         numberOfLab = 1;
-        total.mili_seceonds = 0;
+        total.mili_seconds = 0;
         total.seconds = 0;
         total.minute = 0;
 
         // Save title and remove another child
-        let child = table.lastElementChild;
+        let child: Element | null = table.lastElementChild;
         while(child) {
-            if(table.lastElementChild.className === 'title') {
+            if(child.className === 'title') {
                 break
             } else {
                 table.removeChild(child);
@@ -135,4 +144,4 @@ const lapHandler = () => {
 }
 
 start.addEventListener('click', startHandler);
-lap.addEventListener('click', lapHandler)
+lap.addEventListener('click', lapHandler);
